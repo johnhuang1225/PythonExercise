086d3b13ef0c4b580b7bcb631a8e23ae163201b9
johnhuang <johnhuang1225@gmail.com>
2015-08-15 23:03:11 +0800

    20150815 Requests

---
+++ Practice/src/com/imooc/test.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+'''
+Created on 2015/8/15
+
+@author: John Huang
+'''
+
---
+++ Python27/src/com/imooc/test.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+'''
+Created on 2015/8/15
+
+@author: John Huang
+'''
+import requests
+from bs4 import BeautifulSoup as BS
+res = requests.get("http://www.tdcc.com.tw/smWeb/QryStock.jsp?SCA_DATE=20150807&SqlMethod=StockNo&StockNo=8114&StockName=&sub=%ACd%B8%DF")
+soup = BS(res.text,"html.parser")
+tb = soup.select('.mt')[1]
+for tr in tb.select('tr'):
+    print tr.select('td')[0].text, tr.select('td')[1].text, tr.select('td')[2].text, tr.select('td')[3].text, tr.select('td')[4].text
+
+
+
+
+
